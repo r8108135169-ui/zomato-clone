04233f5e62b57c0de6b716cabe8a56ff99660148
Shiva <r8108135169@gmail.com>
2026-04-30 16:56:15 +0530

    Simplify CORS for production

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,8 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.jsx';
-
+import axios from 'axios';
+axios.defaults.baseURL = 'https://zomato-backend-cajg.onrender.com';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
